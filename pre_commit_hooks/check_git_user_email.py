60ed95493aef6acc0ac3441942a3b763d7608b26
Sebastian Gurkasch <sebastian.gurkasch@t-systems.com>
2025-09-15 14:47:02 +0200

    change domain match

--- pre_commit_hooks/check_git_user_email.py
+++ pre_commit_hooks/check_git_user_email.py
@@ -5,38 +5,30 @@ from collections.abc import Sequence
 from pre_commit_hooks.util import cmd_output
 
 
-def get_git_user_email_global() -> str:
-    """Get the global Git user email."""
-    try:
-        return cmd_output('git', 'config', '--global', 'user.email').strip()
-    except Exception as e:
-        raise RuntimeError('Error: Could not get global Git user email') from e
-
-
 def get_git_user_email_local() -> str:
     """Get the local Git user email."""
     try:
-        return cmd_output('git', 'config', 'user.email').strip()
+        return cmd_output("git", "config", "user.email").strip()
     except Exception as e:
-        raise RuntimeError('Error: Could not get local Git user email') from e
+        raise RuntimeError("Error: Could not get local Git user email") from e
 
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--allowed-domains',
-        nargs='+',
+        "--allowed-domains",
+        nargs="+",
         required=True,
-        help='List of allowed email domains',
+        help="List of allowed email domains",
     )
     args = parser.parse_args(argv)
 
-    for domain in args.allowed_domains:
-        global_email = get_git_user_email_global()
-        local_email = get_git_user_email_local()
+    local_email = get_git_user_email_local()
+    splits = local_email.split("@")
+    used_domain = splits[-1]
 
-        if global_email.endswith(domain) or local_email.endswith(domain):
-            return 0
+    if used_domain in args.allowed_domains:
+        return 0
 
     raise RuntimeError(
         f"""
@@ -49,5 +41,5 @@ def main(argv: Sequence[str] | None = None) -> int:
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
